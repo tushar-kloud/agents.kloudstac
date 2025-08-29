5d78a0e5ac34ced0fdc08bf94817ffdefe7409dd
Tushar <tushar@kloudstac.com>
2025-08-29 10:24:11 +0530

    dev: removed the coming status flag from the agent card

---
+++ src/hooks/auth.ts
@@ -0,0 +1,11 @@
+// import {Auth, Hub} from 'aws-amplify'
+
+// export const signOut = async() => {
+//     try {
+//         // { global: true }
+//         await Auth.signOut();
+//         console.log("User signed out successfully");
+//     } catch (error) {
+//         console.error("Error signing out:", error);
+//     }
+// } 
